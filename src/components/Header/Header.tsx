53771d25ee30a3eff6ce5cb5d279078db32b508e
sergey2421 <sergey_kakadey.@mail.ru>
2025-03-17 16:55:02 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,14 @@ import Refresh from "../../assets/Refresh.tsx";
 import Alert from "../../assets/Alert.tsx";
 
 
-const Header = ({isFetching, error, refresh}: {isFetching: boolean, error: unknown, refresh: void}) => {
+const Header = ({isFetching, error, refresh}: {isFetching: boolean, error: unknown, refresh: () => void}) => {
 
     return (
         <div className="header-container">
             <Logo/>
             <div className="refresh-container">
                 {isFetching ? <div className="fetching">Идет загрузка данных...</div> : error as string && <div className="error"><Alert/>{error as string}</div>}
-                <button className="refresh" onClick={() => refresh}>Обновить <Refresh/></button>
+                <button className="refresh" onClick={refresh}>Обновить <Refresh/></button>
             </div>
         </div>
     )
