3db3073ebd4571363bfc4e902aabf680d8a7ffbe
sergey2421 <sergey_kakadey.@mail.ru>
2025-03-17 17:00:49 +0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: 'https://Sergey2421.github.io/match-tracker/'
+  base: 'https://Sergey2421.github.io/match-tracker/'
 })
